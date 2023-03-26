989a5628f4c06f4fd0c9fced8f02552a42d78a59
Rajan Chaudhary <rajann44@gmail.com>
2023-03-26 21:41:57 +0200

    router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
+import AddMovie from "./components/AddMovie";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Card></Card>
+      <Routes>
+        <Route path="/" element={<Card></Card>}></Route>
+        <Route path="/add" element={<AddMovie></AddMovie>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from "react";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center text-3xl font-bold p-3 border-b-2 border-gray-500 bg-slate-900">
-      <span>
-        Film<span className="text-red-500">Frenzy</span>
-      </span>
-      <h2 className="text-lg flex items-center cursor-pointer">
-        <AddCircleIcon className="mr-1" />
-        Add New
-      </h2>
+      <Link to={"/"}>
+        <span>
+          Film<span className="text-red-500">Frenzy</span>
+        </span>
+      </Link>
+      <Link to={"/add"}>
+        <h2 className="text-lg flex items-center cursor-pointer">
+          <AddCircleIcon className="mr-1" />
+          Add New
+        </h2>
+      </Link>
     </div>
   );
 };
