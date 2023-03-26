a29ebbf2ff1c0b7dc874210657542be592dd8e85
Rajan Chaudhary <rajann44@gmail.com>
2023-03-27 00:58:47 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import Card from "./components/Card";
 import Navbar from "./components/Navbar";
 import AddMovie from "./components/AddMovie";
 import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col min-h-screen">
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Card></Card>}></Route>
         <Route path="/add" element={<AddMovie></AddMovie>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto p-3 border-t-2 border-gray-500 bg-slate-900">
+      <span className="text-3xl flex items-center font-bold">
+        <LiveTvIcon className="mr-1"></LiveTvIcon>
+        <span className="text-red-500">Film</span>
+        Frenzy
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
